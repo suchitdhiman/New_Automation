58e56837e2943c7ec9b5e7488c2bb9230c509f70
skdhi <skdhi@Suchit>
2026-04-13 23:13:54 +0530

    Second-Commit with new project

---
+++ Selenium_Webdriver/src/main/java/com/sk/webdrivers/App.java
@@ -0,0 +1,11 @@
+package com.sk.webdrivers;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("App.main()");
+    }
+}
