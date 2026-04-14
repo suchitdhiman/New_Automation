74b7bfd1830fe93beb090d73da69c60600312391
skdhi <skdhi@Suchit>
2026-04-14 17:53:50 +0530

    added some classes with new methods

--- Selenium_Webdriver/src/main/java/com/sk/webdrivers/App.java
+++ Selenium_Webdriver/src/main/java/com/sk/webdrivers/App.java
@@ -5,7 +5,6 @@ package com.sk.webdrivers;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("App.main()");
+        
     }
 }
---
+++ Selenium_Webdriver/src/main/java/com/sk/webdrivers/TC01.java
@@ -0,0 +1,13 @@
+package com.sk.webdrivers;
+
+public class TC01 extends BaseClass {
+
+	public static void main(String[] args) {
+		
+		browserLauncher("chrome");
+		navigate("https://www.amazon.in/");
+		
+
+	}
+
+}
