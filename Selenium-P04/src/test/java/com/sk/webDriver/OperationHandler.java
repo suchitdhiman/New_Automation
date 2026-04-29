68ae27fe7c36a4c94c6a9ff4a55ad980d95394d9
skdhi <skdhi@Suchit>
2026-04-29 23:32:21 +0530

    Code for selectors with methods

--- Selenium-P04/src/test/java/com/sk/webDriver/OperationHandler.java
+++ Selenium-P04/src/test/java/com/sk/webDriver/OperationHandler.java
@@ -1,16 +1,44 @@
 package com.sk.webDriver;
 
 import java.io.IOException;
+import java.util.Set;
+
+import org.openqa.selenium.WindowType;
 
 public class OperationHandler extends WebDriverManager {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		init();
 		
 		browserLauncher();
 		
 		urlSelect("facebook");
+		
+		Thread.sleep(5000);
+		
+		String tittle = webDriver.getTitle();
+		System.out.println(tittle);
+		
+		String url = webDriver.getCurrentUrl();
+		System.out.println(url);
+		
+		String page = webDriver.getPageSource();
+		//System.out.println(page);
+		
+		String handler = webDriver.getWindowHandle();
+		System.out.println(handler);
+		
+		webDriver.switchTo().newWindow(WindowType.WINDOW);
+		urlSelect("amazon");
+		
+		
+		
+		Set<String> h1d2 = webDriver.getWindowHandles();
+		
+		for(String h:h1d2) {
+			System.out.println(h);
+		}
 		
 
 	}
