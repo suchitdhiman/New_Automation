17df6e959af49e6675eece03b6a4cc0c2f0d5e15
suchitdhiman <dhiman.suchit@gmail.com>
2026-08-28 22:20:37 +0530

    Improved the TestNg

--- TestNG-P010-TestNg/src/test/java/testngpack/NewTest.java
+++ TestNG-P010-TestNg/src/test/java/testngpack/NewTest.java
@@ -1,6 +1,10 @@
 package testngpack;
 
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+import com.sk.webdriver.BaseClass;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -10,18 +14,49 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
-public class NewTest {
+public class NewTest extends BaseClass{
+	
   @Test
-  public void f() {
-	  System.out.println("NewTest.f()");
+  public void actualTest() {
+	  System.out.println("NewTest.actualTest():Starts");
+	  selectOption("amazondropbox_id", "Books");
+		//logger.info("Selected the option <Books> by locating the locator::"+orProperties.getProperty("amazondropbox_id"));
+		extentTest.log(Status.PASS, "Selected the option <Books> by locating the locator::"+orProperties.getProperty("amazondropbox_id"));
+		
+		textType("amazonsearchtextbox_name", "Harry Potter");
+		//logger.info("Typed the text <Harry Potter> by locating the locator::"+orProperties.getProperty("amazonsearchtextbox_name"));
+		extentTest.log(Status.PASS, "Typed the text <Harry Potter> by locating the locator::"+orProperties.getProperty("amazonsearchtextbox_name"));
+		
+		clickElement("amazonsearchbutton_xPath");
+		//logger.info("Clicked on button by locating the locator::"+orProperties.getProperty("amazonsearchbutton_xPath"));
+		extentTest.log(Status.PASS, "Clicked on the button"+orProperties.getProperty("amazonsearchbutton_xPath"));
   }
+  
   @BeforeMethod
-  public void beforeMethod() {
-	  System.out.println("NewTest.beforeMethod()");
+  public void beforeMethod() throws Exception {
+	  	init();
+		//logger.info("Init the properties files.....");
+	  	extentTest = extentReports.createTest("OperationHandler");
+	  	extentTest.log(Status.INFO, "Init the properties files....");
+		
+		browserLauncher();
+		//logger.info("Launched the browser::"+browsProperties.getProperty("chrome"));
+		extentTest.log(Status.INFO,"Launched the browser::"+browsproperties.getProperty("chrome") );
+		
+		selectUrl("amazon");
+		extentTest.log(Status.FAIL, "url selected::"+urlProperties.getProperty("amazon"));
+		
+		webDriver.navigate().refresh();
+		extentTest.log(Status.SKIP, "Skiped! Not tracked");
+		
+		System.out.println("NewTest.beforeMethod()");
+	  
   }
 
   @AfterMethod
   public void afterMethod() {
+	  extentReports.flush();
+	  webDriver.quit();
 	  System.out.println("NewTest.afterMethod()");
   }
 
