b9396654f9b20a899e2c71baba57c429a05f148f
suchitdhiman <dhiman.suchit@gmail.com>
2026-08-28 22:58:22 +0530

    added some testng files

--- TestNG-P010-TestNg/src/test/java/testngpack/NewTest.java
+++ TestNG-P010-TestNg/src/test/java/testngpack/NewTest.java
@@ -1,5 +1,11 @@
 package testngpack;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.Status;
